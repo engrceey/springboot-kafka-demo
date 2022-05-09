49128bc8541663cfe66e9faa7ddb8944a03f0fa0
zurum <zurumogbonda@gmail.com>
2022-05-09 05:39:27 +0100

    add topic config

--- src/main/java/com/kafkademo/demokafka/config/KafkaTopicConfig.java
+++ src/main/java/com/kafkademo/demokafka/config/KafkaTopicConfig.java
@@ -1,4 +1,18 @@
 package com.kafkademo.demokafka.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic kafkaDemoTopic() {
+        return TopicBuilder.name("kafka-demo")
+                .replicas(1)
+                .partitions(3)
+                .build();
+    }
 }
